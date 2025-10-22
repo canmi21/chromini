bc0721dfcf6a516017b33e5d863dd3f331c9335d
Canmi <9997200@qq.com>
2025-10-22 17:36:18 +0800

    style: configure Tailwind CSS to include React components

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* tailwind.config.js */
+
+export default {
+  content: [
+    "./index.html",
+    "./src/renderer/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
