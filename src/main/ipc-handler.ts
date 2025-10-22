de5af8826c12146b40bc98697453cc37c2adde40
Canmi <9997200@qq.com>
2025-10-22 23:22:56 +0800

    feat: create view in focused window using ipc handler

--- src/main/ipc-handler.ts
+++ src/main/ipc-handler.ts
@@ -1,21 +1,22 @@
 /* src/main/ipc-handler.ts */
 
-import { ipcMain } from "electron";
+import { ipcMain, BrowserWindow } from "electron";
 import { createView } from "./view-manager";
-// V-- Import the new function
 import { getConfig, removeHistoryItem } from "./config-manager";
 
 export function setupIpcHandlers() {
 	ipcMain.on("navigate-to-url", (_event, url: string) => {
-		createView(url);
+		// Find the window that sent the request and create the view in it
+		const focusedWindow = BrowserWindow.getFocusedWindow();
+		if (focusedWindow) {
+			createView(url, focusedWindow);
+		}
 	});
 
-	// Provide history to the welcome screen
 	ipcMain.handle("get-history", () => {
 		return getConfig().history;
 	});
 
-	// Handle request to remove a history item
 	ipcMain.on("remove-history-item", (_event, url: string) => {
 		removeHistoryItem(url);
 	});
