5d2438e5917bef09f2af1efd072f24f095983798
Canmi <9997200@qq.com>
2025-10-22 21:33:27 +0800

    chore: remove unnecessary comment in preload script

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -2,7 +2,6 @@
 
 const { contextBridge, ipcRenderer } = require("electron");
 
-// expose api to renderer process
 contextBridge.exposeInMainWorld("electronAPI", {
 	navigateToUrl: (url: string) => {
 		ipcRenderer.send("navigate-to-url", url);
