9fba014595c174b80a430d200499e2b01060796d
Canmi <9997200@qq.com>
2025-10-22 17:39:59 +0800

    chore: update main.tsx with React strict mode

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -1,12 +1,12 @@
 /* src/renderer/main.tsx */
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './app'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
