34023dbea2b03ce464c54c4f2de951e84f0cdab1
Canmi <9997200@qq.com>
2025-10-22 22:42:37 +0800

    feat: add F4 shortcut to close the active view

--- src/main/shortcuts.ts
+++ src/main/shortcuts.ts
@@ -5,9 +5,10 @@ import {
 	showWelcomeView,
 	previousView,
 	nextView,
+	closeActiveView, // Import the new function
 	reloadActiveView,
 	toggleActiveViewDevTools,
-	toggleFullScreen, // Import the new function
+	toggleFullScreen,
 } from "./view-manager";
 
 export function registerShortcuts() {
@@ -16,6 +17,7 @@ export function registerShortcuts() {
 	globalShortcut.register("F1", showWelcomeView);
 	globalShortcut.register("F2", previousView);
 	globalShortcut.register("F3", nextView);
+	globalShortcut.register("F4", closeActiveView); // Add F4 shortcut
 	globalShortcut.register("F5", reloadActiveView);
 	globalShortcut.register("F11", toggleFullScreen);
 	globalShortcut.register("F12", toggleActiveViewDevTools);
