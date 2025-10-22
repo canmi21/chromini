8a2ee1f65ceec27d9d38668a62a2d03aebcb6b26
Canmi <9997200@qq.com>
2025-10-22 19:48:58 +0800

    fix: stop tracking dist-electron directory

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,72 +1,96 @@
 /* dist-electron/main.js */
 
-import { ipcMain, app, BrowserWindow } from "electron";
+import { app, ipcMain, BrowserWindow, globalShortcut } from "electron";
 import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const isDev = process.env.NODE_ENV === "development";
+const isDev = process.env.NODE_ENV === "development" || !app.isPackaged;
 let mainWindow = null;
 let browserWindow = null;
 function createMainWindow() {
-	mainWindow = new BrowserWindow({
-		width: 600,
-		height: 400,
-		resizable: false,
-		webPreferences: {
-			preload: path.join(__dirname, "../preload/preload.js"),
-			contextIsolation: true,
-			nodeIntegration: false,
-		},
-	});
-	if (isDev) {
-		mainWindow.loadURL("http://localhost:5173");
-	} else {
-		mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
-	}
-	mainWindow.on("closed", () => {
-		mainWindow = null;
-	});
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+      nodeIntegration: false,
+      webSecurity: true
+    }
+  });
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:5173");
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 }
 function createBrowserWindow(url) {
-	if (mainWindow) {
-		mainWindow.close();
-	}
-	browserWindow = new BrowserWindow({
-		width: 1200,
-		height: 800,
-		webPreferences: {
-			nodeIntegration: false,
-			contextIsolation: true,
-			devTools: true,
-		},
-	});
-	browserWindow.loadURL(url);
-	browserWindow.webContents.on("before-input-event", (_event, input) => {
-		if (input.key === "F12") {
-			browserWindow == null
-				? void 0
-				: browserWindow.webContents.toggleDevTools();
-		}
-	});
-	browserWindow.on("closed", () => {
-		browserWindow = null;
-		app.quit();
-	});
+  if (mainWindow) {
+    mainWindow.close();
+  }
+  browserWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      devTools: true,
+      webSecurity: true
+    }
+  });
+  browserWindow.loadURL(url).catch((err) => {
+    console.error("Failed to load URL:", err);
+  });
+  const registerDevToolsShortcuts = () => {
+    globalShortcut.register("F12", () => {
+      if (browserWindow && !browserWindow.isDestroyed()) {
+        browserWindow.webContents.toggleDevTools();
+      }
+    });
+    if (process.platform === "darwin") {
+      globalShortcut.register("Command+Option+I", () => {
+        if (browserWindow && !browserWindow.isDestroyed()) {
+          browserWindow.webContents.toggleDevTools();
+        }
+      });
+    } else {
+      globalShortcut.register("Control+Shift+I", () => {
+        if (browserWindow && !browserWindow.isDestroyed()) {
+          browserWindow.webContents.toggleDevTools();
+        }
+      });
+    }
+  };
+  registerDevToolsShortcuts();
+  browserWindow.on("closed", () => {
+    globalShortcut.unregisterAll();
+    browserWindow = null;
+    app.quit();
+  });
 }
 ipcMain.on("navigate-to-url", (_event, url) => {
-	createBrowserWindow(url);
+  createBrowserWindow(url);
 });
 app.whenReady().then(() => {
-	createMainWindow();
-	app.on("activate", () => {
-		if (BrowserWindow.getAllWindows().length === 0) {
-			createMainWindow();
-		}
-	});
+  createMainWindow();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
 });
 app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  globalShortcut.unregisterAll();
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll();
 });
