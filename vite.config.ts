8bf2f8992bc0dae7bc073a4a343054e90c590ebd
Canmi <9997200@qq.com>
2025-10-22 17:40:00 +0800

    chore: update vite.config.ts plugins and build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,47 +1,47 @@
 /* vite.config.ts */
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import electron from "vite-plugin-electron";
+import renderer from "vite-plugin-electron-renderer";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    electron([
-      {
-        entry: 'src/main/main.ts',
-        vite: {
-          build: {
-            outDir: 'dist-electron',
-            rollupOptions: {
-              external: ['electron']
-            }
-          }
-        }
-      },
-      {
-        entry: 'src/preload/preload.ts',
-        onstart(options) {
-          options.reload()
-        },
-        vite: {
-          build: {
-            outDir: 'dist-electron',
-            rollupOptions: {
-              external: ['electron']
-            }
-          }
-        }
-      }
-    ]),
-    renderer()
-  ],
-  base: './',
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    port: 5173,
-  },
-})
+	plugins: [
+		react(),
+		electron([
+			{
+				entry: "src/main/main.ts",
+				vite: {
+					build: {
+						outDir: "dist-electron",
+						rollupOptions: {
+							external: ["electron"],
+						},
+					},
+				},
+			},
+			{
+				entry: "src/preload/preload.ts",
+				onstart(options) {
+					options.reload();
+				},
+				vite: {
+					build: {
+						outDir: "dist-electron",
+						rollupOptions: {
+							external: ["electron"],
+						},
+					},
+				},
+			},
+		]),
+		renderer(),
+	],
+	base: "./",
+	build: {
+		outDir: "dist",
+	},
+	server: {
+		port: 5173,
+	},
+});
