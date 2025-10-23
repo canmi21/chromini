96d62f42429987663d2689b0f3c458a991bd92d0
Canmi <9997200@qq.com>
2025-10-23 19:41:43 +0800

    refactor: remove unused code and comments in main.ts

--- src/main/main.ts
+++ src/main/main.ts
@@ -4,22 +4,19 @@ import { app, session } from "electron";
 import { setupIpcHandlers } from "./ipc-handler";
 import { registerShortcuts, unregisterShortcuts } from "./shortcuts";
 import { createMainWindow, getWindowCount } from "./window-manager";
-import { createAppMenu } from "./app-menu"; // Import the new menu creator
+import { createAppMenu } from "./app-menu";
 
 app.whenReady().then(() => {
-	// Get the default user agent string.
 	const userAgent = session.defaultSession.getUserAgent();
 	const chromeUserAgent = userAgent.replace(/Electron\/[\d\.]+\s/, "");
-	// Set the modified user agent for all future requests.
 	session.defaultSession.setUserAgent(chromeUserAgent);
 
 	setupIpcHandlers();
 	registerShortcuts();
-	createAppMenu(); // Set the application menu
+	createAppMenu();
 	createMainWindow();
 
 	app.on("activate", () => {
-		// On macOS, re-create a window when the dock icon is clicked
 		if (getWindowCount() === 0) {
 			createMainWindow();
 		}
@@ -27,13 +24,11 @@ app.whenReady().then(() => {
 });
 
 app.on("window-all-closed", () => {
-	// Quit when all windows are closed, except on macOS
 	if (process.platform !== "darwin") {
 		app.quit();
 	}
 });
 
 app.on("will-quit", () => {
-	// Unregister all shortcuts on quit
 	unregisterShortcuts();
 });
